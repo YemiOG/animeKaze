2092ce6b0d08d03eea297226dbd836d01688cc85
Faruq <faruqabdulsalam@yahoo.com>
2021-12-04 21:37:30 +0100

    Create user model

--- backend/anime/models.py
+++ backend/anime/models.py
@@ -12,7 +12,7 @@ followers = db.Table('followers',
                      )
 
 
-class User(UserMixin, BaseModel):
+class User(db.Model, UserMixin):
     __tablename__ = 'user'
 
     id = db.Column(db.Integer, primary_key=True)  # primary keys are required
@@ -58,7 +58,6 @@ class User(UserMixin, BaseModel):
         own = Post.query.filter_by(user_id=self.id)
         return followed.union(own).order_by(Post.timestamp.desc())
 
-
 class Post(BaseModel):
     id = db.Column(db.Integer, primary_key=True)
     recipe = db.Column(db.String(360))
@@ -78,3 +77,70 @@ def load_user(user_id):
     # since the user_id is just the primary key of our user table, use it in
     # the query for the user
     return User.query.get(int(user_id))
+
+# class User(UserMixin, BaseModel):
+#     __tablename__ = 'user'
+
+#     id = db.Column(db.Integer, primary_key=True)  # primary keys are required
+#     # by SQLAlchemy
+#     name = db.Column(db.String(64), index=True)
+#     username = db.Column(db.String(64), index=True, unique=True)
+#     email = db.Column(db.String(120), index=True, unique=True)
+#     password_hash = db.Column(db.String(128))
+#     posts = db.relationship('Post', backref='author', lazy='dynamic')
+#     about_me = db.Column(db.String(140))
+#     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+#     followed = db.relationship(
+#         'User', secondary=followers,
+#         primaryjoin=(followers.c.follower_id == id),
+#         secondaryjoin=(followers.c.followed_id == id),
+#         backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
+
+#     def __repr__(self):
+#         return '<User {}>'.format(self.username)
+
+#     def follow(self, user):
+#         if not self.is_following(user):
+#             self.followed.append(user)
+
+#     def unfollow(self, user):
+#         if self.is_following(user):
+#             self.followed.remove(user)
+
+#     def is_following(self, user):
+#         return self.followed.filter(
+#             followers.c.followed_id == user.id).count() > 0
+
+#     def set_password(self, password):
+#         self.password_hash = generate_password_hash(password)
+
+#     def check_password(self, password):
+#         return check_password_hash(self.password_hash, password)
+
+#     def followed_posts(self):
+#         followed = Post.query.join(
+#             followers, (followers.c.followed_id == Post.user_id)).filter(
+#                 followers.c.follower_id == self.id)
+#         own = Post.query.filter_by(user_id=self.id)
+#         return followed.union(own).order_by(Post.timestamp.desc())
+
+
+# class Post(BaseModel):
+#     id = db.Column(db.Integer, primary_key=True)
+#     recipe = db.Column(db.String(360))
+#     ingredients = db.Column(db.String(360))
+#     cookingSteps = db.Column(db.String(360))
+#     cookingTime = db.Column(db.String(10))
+#     image = db.Column(db.String(360))
+#     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+#     def __repr__(self):
+#         return '<Post {}>'.format(self.recipe)
+
+
+# @login_manager.user_loader
+# def load_user(user_id):
+#     # since the user_id is just the primary key of our user table, use it in
+#     # the query for the user
+#     return User.query.get(int(user_id))
