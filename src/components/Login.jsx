8d4c1e29808a20557a64e1973a86e0ba7b5c60ab
Faruq <faruqabdulsalam@yahoo.com>
2021-12-10 09:56:31 +0100

    Add api call to get toekn

---
+++ src/components/Login.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from 'react';
+import axios from "axios";
+
+function Login(props) {
+  //  const token = sessionStorage.getItem("token")
+  //  console.log(token)
+
+    const [loginForm, setloginForm] = useState({
+      email: "",
+      password: ""
+    })
+
+    function logMeIn(event) {
+      axios({
+        method: "POST",
+        url:"/api/token",
+        data:{
+          email: loginForm.email,
+          password: loginForm.password
+         }
+      })
+      .then((response) => {
+        props.setToken(response.data.access_token)
+      }).catch((error) => {
+        if (error.response) {
+          console.log(error.response)
+          console.log(error.response.status)
+          console.log(error.response.headers)
+          }
+      })
+  
+      setloginForm(({
+        email: "",
+        password: ""}))
+  
+      event.preventDefault()
+    }
+    
+    function handleChange(event) { 
+      const {value, name} = event.target
+      setloginForm(prevNote => ({
+          ...prevNote, [name]: value})
+      )}
+
+    return (
+      <div>
+        <h1>Login</h1>
+        {/* {token && token!=="" &&token!== undefined ? 
+        ("You are logged in")
+        :( */}
+          <form className="login">
+            <input onChange={handleChange} 
+                  type="email"
+                  text={loginForm.email} 
+                  name="email" 
+                  placeholder="Email" 
+                  value={loginForm.email} />
+            <input onChange={handleChange} 
+                  type="password"
+                  text={loginForm.password} 
+                  name="password" 
+                  placeholder="Password" 
+                  value={loginForm.password} />
+          
+          <button onClick={logMeIn}>Submit</button>
+        </form>
+        {/* )} */}
+      </div>
+    );
+}
+
+export default Login;
