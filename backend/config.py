70b09c244707340a08917c7852b793d60cfc1e67
Faruq <faruqabdulsalam@yahoo.com>
2021-12-10 09:54:02 +0100

    Add JWT configuration

--- backend/config.py
+++ backend/config.py
@@ -1,9 +1,9 @@
 import os
 from dotenv import load_dotenv 
+from datetime import timedelta
 
 load_dotenv()
 basedir = os.path.abspath(os.path.dirname(__file__))
-
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
@@ -13,3 +13,7 @@ class Config(object):
     UPLOAD_FOLDER = os.path.join(basedir, 'anime/static/uploads')
     ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'you-will-never-know'
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=int(os.environ.get('JWT_ACCESS_TOKEN_EXPIRES')))
+
+
