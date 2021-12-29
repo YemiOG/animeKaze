67879bcdbd2b4e9b33419b9539329b8a420c10ac
Faruq <faruqabdulsalam@yahoo.com>
2021-12-29 21:08:37 +0100

    Add error page

---
+++ src/components/error/pageNotFound.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function PageNotFound() {
+    return(
+    <div className="container-page">
+      <div className="row">
+          <div className="col-md-6 offset-md-3 col-xs-12">
+             <h1>Not Found</h1>
+             <p>Sorry! The page you are looking for does not exist</p>
+          </div>
+      </div>
+    </div>  
+    )
+}
+
+export default PageNotFound
