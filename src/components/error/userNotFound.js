3c32e5f335b671c8b12f574b1f6553bc071f6d29
Faruq <faruqabdulsalam@yahoo.com>
2021-12-29 21:08:45 +0100

    Add error page

---
+++ src/components/error/userNotFound.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function userNotFound() {
+    return(
+    <div className="container-page">
+      <div className="row">
+          <div className="col-md-6 offset-md-3 col-xs-12">
+             <h1>Not Found</h1>
+             <p>Sorry! User not found</p>
+          </div>
+      </div>
+    </div>  
+    )
+}
+
+export default userNotFound
