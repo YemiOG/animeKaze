b2adb81ad2a28d446d6c82e0598dcae8459d12a6
Faruq <faruqabdulsalam@yahoo.com>
2021-12-29 21:08:01 +0100

    Created authentication route component

---
+++ src/components/Auth/AuthedRoute.js
@@ -0,0 +1,15 @@
+import {useContext} from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+import { UserContext } from '../contexts/userContext';
+
+function AuthedRoute( {children}:{children : Component}) {
+        let location = useLocation();
+        const {token} = useContext(UserContext);
+        if(!token && token!=="" && token !== undefined)
+        {
+            return <Navigate to="/login" state={{from: location}} />
+        }
+        return children
+    }
+
+export default AuthedRoute;
