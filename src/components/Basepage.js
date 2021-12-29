d24797ca024058396c57585607cdbaa1da847ae9
Faruq <faruqabdulsalam@yahoo.com>
2021-12-29 23:26:32 +0100

    Create basepage component

---
+++ src/components/Basepage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Base(){
+
+    return (
+        <>
+            <div className="note">
+                <h1 >  Welcome to AnimeKaze </h1>
+            </div>
+        </>
+    )
+}
+
+export default Base;
