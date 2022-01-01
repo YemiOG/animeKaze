1186c44bb82de38c1a99de6cda452095c01dfe48
Faruq <faruqabdulsalam@yahoo.com>
2022-01-01 17:15:37 +0100

    Create eamil functions

---
+++ backend/anime/api/email.py
@@ -0,0 +1,17 @@
+from flask_mail import Message
+from .. import mail
+from flask import render_template
+
+def send_email(subject, recipients, text_body):
+    msg = Message(subject, recipients=recipients)
+    msg.body = text_body
+    mail.send(msg)
+
+def send_password_reset_email(user):
+	token = user.get_reset_password_token()
+	print(token)
+	print(user.email)
+	send_email('[animeKaze] Reset Your Password',
+				recipients=[user.email],
+				text_body=render_template('reset_password.txt',
+				user=user, token=token))
