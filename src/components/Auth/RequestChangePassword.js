8f8581ef90a96ac3107f7072e8f08f6ca532a003
Faruq <faruqabdulsalam@yahoo.com>
2022-01-02 03:15:11 +0100

    Create request password reset component

---
+++ src/components/Auth/RequestChangePassword.js
@@ -0,0 +1,57 @@
+import React,{useState,useContext} from 'react';
+import axios from "axios";
+import { UserContext } from '../contexts/userContext';
+
+function RequestPasswordChange() {
+
+	const [email , setNewEmail] = useState("")
+	const [noUser, setNoUser] = useState("")
+	const {token} = useContext(UserContext);
+
+	function handleChange(event) {
+        const newValue = event.target.value
+        setNewEmail(newValue);
+    }
+
+	function Recover(event) {
+		axios({
+			method: "POST",
+			url:"/api/reset_password_request",
+			data:{
+			  email: email,
+			  token:token,
+			 }
+		})
+		.then((response) => {
+			console.log(response)
+		}).catch((error) => {
+			if (error.response) {
+				if (error.response.status === 400){
+					setNoUser(error.response.data.message)
+				}
+			}
+		})
+		setNewEmail("")
+		event.preventDefault()
+	}
+
+    return (
+		<div className="profile-page">
+			<p>Trouble with logging in?</p>
+			<p>Enter your email address and we'll send you a link to get back into your account.</p>
+			<form className="login">
+            <input onChange={handleChange} 
+                type="email"
+                text={email} 
+                name="email" 
+                placeholder="Email" 
+                value={email} />
+          	<button onClick={Recover}>Submit</button>
+        	</form>
+			{noUser && <p>{noUser}</p>}
+		</div>
+    );
+  };
+
+export default RequestPasswordChange;
+
