15ae67dc1b5ab91626aa45024f7686700c1aa25d
Faruq <faruqabdulsalam@yahoo.com>
2022-01-02 03:17:55 +0100

    Add post component to homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -9,14 +9,13 @@ function Home(){
 	// const [postMessage, setpostMessage] = useState("")
     const [posts, setPosts] = useState("")
 	const [content, setContent] = useState("")
-    const {token, removeToken, setAppState}= useContext(UserContext);
+    const {token, removeToken, setAppState}= useContext(UserContext)
 	const username = window.localStorage.getItem('username')
 	const userId = JSON.parse(window.localStorage.getItem("cuid"))
 
     useEffect(() => {
 		// setAppState({ loading: true });
         getPosts()
-		// console.log("here")
     },[])
 
 	function handleChange(event) { 
@@ -28,7 +27,7 @@ function Home(){
 		console.log(username)
 		axios({
 			method: "GET",
-			url:'/api/'+ username + '/posts',
+			url:'/api/home/'+ username + '/posts',
 			headers: {
 			  Authorization: 'Bearer ' + token
 			}
@@ -53,7 +52,10 @@ function Home(){
 		axios({
 			method: "POST",
 			url: '/api/upload',
-			data:formData
+			data:formData,
+			headers: {
+						Authorization: 'Bearer ' + token
+			  		}
 			}).then((response)=>{
 				getPosts() // get posts upon successful post submission
 			  }).catch((error) => {
@@ -68,6 +70,9 @@ function Home(){
 		
     return (
         <>
+			<div className="note">
+                <h1 >  Welcome to AnimeKaze </h1>
+            </div>
             <Search />
 			<form onSubmit={submitForm} encType="multipart/form-data" className="create-note">
 				<input  type="text" onChange={handleChange} name="content" placeholder="What's happening?" value={content} required/>
@@ -79,9 +84,6 @@ function Home(){
 					Post
 				</button>
 			</form>
-            <div className="note">
-                <h1 >  Welcome to AnimeKaze </h1>
-            </div>
 			{posts && posts.map(posts => <Posts key={posts.id} content={posts.content} image={posts.image}/>)}
         </>
     )
