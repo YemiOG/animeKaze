7eb416fc6182aca039969766f257046566b4e58b
Faruq <faruqabdulsalam@yahoo.com>
2022-01-15 10:07:56 +0100

    Add like functionality option to posts

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,8 +1,13 @@
 function Posts(props){
+    function handleClick(){
+        props.like(props.id);
+      }
 	return (
             <div className="note">
                 <h1 >  {props.content} </h1>
 				<img alt={""} src={props.image} />
+                <p> {props.likeCount} </p>
+                <button onClick={handleClick}> Like </button>
             </div>
     )
 }
