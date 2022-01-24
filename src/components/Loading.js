acf3fb0f9bc249dd1999cad2165b5969ff862e11
Faruq <faruqabdulsalam@yahoo.com>
2022-01-24 20:37:26 +0100

    Create loading component

---
+++ src/components/Loading.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useContext } from 'react';
+import { UserContext } from './contexts/userContext';
+
+function Loading(Component) {
+  return function LoadingComponent(props) {
+	const {appState} = useContext(UserContext);
+
+    if (!appState.loading) return <Component {...props}/>;
+    return (
+      <p style={{ textAlign: 'center', fontSize: '30px' }}>
+        Hold on, fetching data may take some time :)
+      </p>
+    );
+  };
+}
+export default Loading;
