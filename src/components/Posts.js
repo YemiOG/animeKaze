a33ffb7a5d47fa6aeaad225785703323ea00d77f
Faruq <faruqabdulsalam@yahoo.com>
2022-01-26 02:32:39 +0100

    Add comments components to Post component

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,3 +1,5 @@
+import Comments from "./Comments"
+
 function Posts(props){
     function handleClick(){
         props.like(props.id);
@@ -9,14 +11,19 @@ function Posts(props){
         props.interested(props.id);
       }
 	return (
-            <div className="note">
-                <h1 >  {props.content} </h1>
-				        <img alt={""} src={props.image} />
-                <p> {props.likeCount} </p>
-                <button onClick={handleClick}> Like </button>
-                {props.report && <button onClick={handleReport}> Report </button>}
-                {props.interested && <button onClick={handleInterest}> Not interested </button>}
-            </div>
+        <div>
+          <div className="note">
+              <h1 >  {props.content} </h1>
+				      <img alt={""} src={props.image} />
+              <p> {props.likeCount} </p>
+              <button onClick={handleClick}> Like </button>
+              {props.report && <button onClick={handleReport}> Report </button>}
+              {props.interested && <button onClick={handleInterest}> Not interested </button>}
+          </div>
+          <div>
+            <Comments postId={props.id}/>
+          </div>
+        </div>
     )
 }
 
