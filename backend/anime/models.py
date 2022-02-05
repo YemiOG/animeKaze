adde73d64b03f406df0b584fdf187cf07faa194c
Faruq <faruqabdulsalam@yahoo.com>
2022-02-05 19:29:42 +0100

    Use flake8 to clean up code

--- backend/anime/models.py
+++ backend/anime/models.py
@@ -1,41 +1,38 @@
-import os
-import base64
 import jwt
-import random
 from anime import app
 from time import time
 from flask_login import UserMixin
 from flask import url_for
-from sqlalchemy import and_,not_, or_
+from sqlalchemy import or_
 from sqlalchemy.sql import func
 from werkzeug.security import generate_password_hash, check_password_hash
-from datetime import datetime
 from . import db, login_manager
 from datetime import datetime, timedelta
 
-#table for followed and following users
+# table for followed and following users
 followers = db.Table('followers',
                      db.Column('follower_id', db.Integer,
                                db.ForeignKey('user.id')),
                      db.Column('followed_id',
                                db.Integer, db.ForeignKey('user.id'))
-                    )
+                     )
 
-#table for posts user is not interested in
+# table for posts user is not interested in
 notInterested = db.Table('notInterested',
-                     db.Column('post_id', db.Integer,
-                               db.ForeignKey('post.id')),
-                     db.Column('person_id', db.Integer,
-                                db.ForeignKey('user.id'))
-                    )
+                         db.Column('post_id', db.Integer,
+                                   db.ForeignKey('post.id')),
+                         db.Column('person_id', db.Integer,
+                                   db.ForeignKey('user.id'))
+                         )
 
-#table for posts reported by user
+# table for posts reported by user
 reportedPost = db.Table('reportedPost',
-                     db.Column('post_id', db.Integer,
-                               db.ForeignKey('post.id')),
-                     db.Column('person_id', db.Integer,
-                                db.ForeignKey('user.id'))
-                    )
+                        db.Column('post_id', db.Integer,
+                                  db.ForeignKey('post.id')),
+                        db.Column('person_id', db.Integer,
+                                  db.ForeignKey('user.id'))
+                        )
+
 
 class PaginatedAPIMixin(object):
     @staticmethod
@@ -59,7 +56,8 @@ class PaginatedAPIMixin(object):
             }
         }
         return data
-        
+
+
 class User(PaginatedAPIMixin, db.Model, UserMixin):
     __tablename__ = 'user'
 
@@ -73,7 +71,8 @@ class User(PaginatedAPIMixin, db.Model, UserMixin):
     avatar = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     comment = db.relationship('Comment', backref='author', lazy='dynamic')
-    child_comment = db.relationship('ChildComment', backref='author', lazy='dynamic')
+    child_comment = db.relationship('ChildComment', backref='author',
+                                    lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
     followed = db.relationship(
@@ -90,7 +89,7 @@ class User(PaginatedAPIMixin, db.Model, UserMixin):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
-    
+
     def follow(self, user):
         if not self.is_following(user):
             self.followed.append(user)
@@ -105,39 +104,44 @@ class User(PaginatedAPIMixin, db.Model, UserMixin):
 
     def followed_posts(self):
 
-        #get count of reported posts by post id 
+        # get count of reported posts by post id
         reported_count = db.session.query(
-                            reportedPost.c.post_id, func.count('*').label('reporting')
-                            ).group_by(reportedPost.c.post_id).subquery()
+                            reportedPost.c.post_id, func.count('*').label(
+                             'reporting')).group_by(
+                             reportedPost.c.post_id).subquery()
 
-        #get posts from user's followers minus reported posts
+        # get posts from user's followers minus reported posts
         followed = db.session.query(Post).join(
             followers, (followers.c.followed_id == Post.user_id)).outerjoin(
                 reported_count).outerjoin(reportedPost).filter(
                 followers.c.follower_id == self.id).filter(
-                or_(reportedPost.c.person_id != self.id, reportedPost.c.person_id == None)).filter(
-                    or_(reported_count.c.reporting < 2 , reportedPost.c.person_id == None))
+                or_(reportedPost.c.person_id != self.id,
+                    reportedPost.c.person_id == None)).filter(
+                    or_(reported_count.c.reporting < 2, reportedPost.c.person_id == None))
 
-        #get user's own posts 
+        # get user's own posts
         own = Post.query.filter_by(user_id=self.id)
 
-        #combine and return both filtered post queries
-        return followed.union(own).order_by(Post.timestamp.desc(), func.random())
-    
+        # combine and return both filtered post queries
+        return followed.union(own).order_by(Post.timestamp.desc())
+        # return followed.union(own).order_by(Post.timestamp.desc(), func.random())
+
     def filter_posts(self):
-        #get count of not interested posts by post id 
+        # get count of not interested posts by post id
         no_intrst_count = db.session.query(
-                            notInterested.c.post_id, func.count('*').label('not_interest_count')
-                            ).group_by(notInterested.c.post_id).subquery()
-        #filter out posts user doesn't want to see
-        explore= db.session.query(Post).filter(Post.user_id != self.id).outerjoin(
+                            notInterested.c.post_id, func.count('*').label(
+                             'not_interest_count')).group_by(
+                                notInterested.c.post_id).subquery()
+        # filter out posts user doesn't want to see
+        explore = db.session.query(Post).filter(
+            Post.user_id != self.id).outerjoin(
             no_intrst_count).outerjoin(notInterested).filter(
                 or_(notInterested.c.person_id != self.id, notInterested.c.person_id == None)).filter(
-                    or_(no_intrst_count.c.not_interest_count < 2 , notInterested.c.person_id == None))
+                    or_(no_intrst_count.c.not_interest_count < 2,
+                        notInterested.c.person_id == None))
 
         return explore.order_by(Post.timestamp.desc())
-    
-    
+
     def get_reset_password_token(self, expires_in=600):
         return jwt.encode(
             {'reset_password': self.id, 'exp': time() + expires_in},
@@ -151,7 +155,6 @@ class User(PaginatedAPIMixin, db.Model, UserMixin):
         except:
             return
         return User.query.get(id)
-
 
     def to_dict(self):
         data = {
@@ -179,17 +182,19 @@ class User(PaginatedAPIMixin, db.Model, UserMixin):
                 setattr(self, field, data[field].lower())
         if new_user and 'password' in data:
             self.set_password(data['password'])
-    
+
     def revoke_token(self):
         self.token_expiration = datetime.utcnow() - timedelta(seconds=1)
 
-#table for liked posts
+
+# table for liked posts
 likedPosts = db.Table('likedPosts',
-                     db.Column('post_id', db.Integer,
-                               db.ForeignKey('post.id')),
-                     db.Column('liker_id', db.Integer,
+                      db.Column('post_id', db.Integer,
+                                db.ForeignKey('post.id')),
+                      db.Column('liker_id', db.Integer,
                                 db.ForeignKey('user.id'))
-                    )
+                      )
+
 
 class Post(PaginatedAPIMixin, db.Model):
     __tablename__ = "post"
@@ -198,13 +203,13 @@ class Post(PaginatedAPIMixin, db.Model):
     content = db.Column(db.String(360))
     image = db.Column(db.String(360))
     comments = db.relationship('Comment', backref='post', lazy='dynamic')
-    likes= db.relationship(
+    likes = db.relationship(
         'User', secondary=likedPosts,
         backref=db.backref('likedPosts', lazy='dynamic'), lazy='dynamic')
-    not_interested= db.relationship(
+    not_interested = db.relationship(
         'User', secondary=notInterested,
         backref=db.backref('notInterested', lazy='dynamic'), lazy='dynamic')
-    reported= db.relationship(
+    reported = db.relationship(
         'User', secondary=reportedPost,
         backref=db.backref('reportedPost', lazy='dynamic'), lazy='dynamic')
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
@@ -212,22 +217,22 @@ class Post(PaginatedAPIMixin, db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.content)
-    
+
     def like_state(self, user):
         if not self.liked(user):
             self.likes.append(user)
         else:
             self.likes.remove(user)
-    
+
     def liked(self, user):
         return self.likes.filter(likedPosts.c.liker_id == user.id).count() > 0
-    
+
     def no_interest(self, user):
         if not self.interested(user):
             self.not_interested.append(user)
         else:
             self.not_interested.remove(user)
-    
+
     def interested(self, user):
         return self.not_interested.filter(notInterested.c.person_id == user.id).count() > 0
 
@@ -240,7 +245,7 @@ class Post(PaginatedAPIMixin, db.Model):
 
     def post_reported(self, user):
         return self.reported.filter(reportedPost.c.person_id == user.id).count() > 0
-    
+
     def to_dict(self):
         data = {
             'id': self.id,
@@ -250,20 +255,23 @@ class Post(PaginatedAPIMixin, db.Model):
         }
         return data
 
-#table for liked comments
+
+# table for liked comments
 likedComments = db.Table('likedComments',
-                     db.Column('comment_id', db.Integer,
-                               db.ForeignKey('comment.id')),
-                     db.Column('liker_id', db.Integer,
-                                db.ForeignKey('user.id'))
-                    )
+                         db.Column('comment_id', db.Integer,
+                                   db.ForeignKey('comment.id')),
+                         db.Column('liker_id', db.Integer,
+                                   db.ForeignKey('user.id'))
+                         )
+
+
 class Comment(PaginatedAPIMixin, db.Model):
     __tablename__ = "comment"
 
     id = db.Column(db.Integer, primary_key=True)
-    content= db.Column(db.String(360))
+    content = db.Column(db.String(360))
     comments = db.relationship('ChildComment', backref='comment', lazy='dynamic')
-    likes= db.relationship(
+    likes = db.relationship(
             'User', secondary=likedComments,
             backref=db.backref('likedComments', lazy='dynamic'), lazy='dynamic')
     timestamps = db.Column(db.DateTime, index=True, default=datetime.utcnow)
@@ -273,16 +281,16 @@ class Comment(PaginatedAPIMixin, db.Model):
 
     def __repr__(self):
         return '<Comment from user {}>'.format(self.user_id)
-    
+
     def like_comment_state(self, user):
         if not self.liked_comment(user):
             self.likes.append(user)
         else:
             self.likes.remove(user)
-    
+
     def liked_comment(self, user):
         return self.likes.filter(likedComments.c.liker_id == user.id).count() > 0
-    
+
     def to_dict(self):
         user = db.session.query(User).filter_by(id=self.user_id).all()
         data = {
@@ -296,20 +304,22 @@ class Comment(PaginatedAPIMixin, db.Model):
         return data
 
 
-#table for liked child comments
+# table for liked child comments
 likedChildComments = db.Table('likedChildComments',
-                     db.Column('comment_id', db.Integer,
-                               db.ForeignKey('childcomment.id')),
-                     db.Column('liker_id', db.Integer,
-                                db.ForeignKey('user.id'))
-                            )
+                              db.Column('comment_id', db.Integer,
+                                        db.ForeignKey('childcomment.id')),
+                              db.Column('liker_id', db.Integer,
+                                        db.ForeignKey('user.id'))
+                              )
+
+
 class ChildComment(PaginatedAPIMixin, db.Model):
     __tablename__ = "childcomment"
 
     id = db.Column(db.Integer, primary_key=True)
-    content= db.Column(db.String(360))
+    content = db.Column(db.String(360))
     comment_id = db.Column(db.Integer, db.ForeignKey('comment.id'))
-    likes= db.relationship(
+    likes = db.relationship(
             'User', secondary=likedChildComments,
             backref=db.backref('likedChildComments', lazy='dynamic'), lazy='dynamic')
     timestamps = db.Column(db.DateTime, index=True, default=datetime.utcnow)
@@ -318,16 +328,16 @@ class ChildComment(PaginatedAPIMixin, db.Model):
 
     def __repr__(self):
         return '<Child Comment from user {}>'.format(self.user_id)
-    
+
     def like_child_comment(self, user):
         if not self.liked_comment(user):
             self.likes.append(user)
         else:
             self.likes.remove(user)
-    
+
     def liked_comment(self, user):
         return self.likes.filter(likedChildComments.c.liker_id == user.id).count() > 0
-    
+
     def to_dict(self):
         user = db.session.query(User).filter_by(id=self.user_id).all()
         data = {
