26a682a965b9854b61cfe7bd419f93bdafa3884f
Faruq <faruqabdulsalam@yahoo.com>
2022-02-14 20:57:18 +0100

    Add emoji picker to create post page

--- src/components/posts/createPost.js
+++ src/components/posts/createPost.js
@@ -9,19 +9,28 @@ import emoji from '../../images/emoji.png'
 function CreatePost(props){
 
 	const [content, setContent] = useState("")
+	const [image, setImage] = useState(null);
 	const [textArea, setTextArea] = useState(false)
 	const avatar = window.localStorage.getItem('avatar')
 	const userId = JSON.parse(window.localStorage.getItem("cuid"))
 	const {token, removeToken}= useContext(UserContext)
-
-	// function handleChange(event) { 
-	// 	const post = event.target.value
-	// 	setContent(post)
-	// }
-
+	
 	function expandTextArea(){
 		setTextArea(true)
 	}
+	
+	function onChangeFile(event){
+		(
+			(event.target.files && (event.target.files[0] != null)) &&
+			setImage(URL.createObjectURL(event.target.files[0]))
+		)
+	}
+
+	function cancelPost(){
+		setImage(null)
+		setTextArea(false)
+	}
+
 
 	function submitForm (event){
 		const formData = new FormData(event.target)
@@ -47,42 +56,49 @@ function CreatePost(props){
 		event.preventDefault()
 	}
 
-	// chosenEmoji && (content+=chosenEmoji)
-	// console.log(content)
-	// console.log(chosenEmoji)
-
 	return (
-		<>
-		<form onSubmit={submitForm} encType="multipart/form-data" className="post-form">
-			<div className="formTop">
-				<div className='profileImage'>
-					<img src={avatar} alt="profile logo"/>
+		<div className="createPost">
+			{textArea && <div className="post-top">
+					<p>Create a post</p>
+					<div onClick={cancelPost}> X </div>
 				</div>
-				{textArea ? <InputEmoji
-					className='text'
-					value={content}
-					onChange={setContent}
-					placeholder="What's happening?"
-					rows='3'
-					required
-				/>
-				:
-				<input onClick={expandTextArea} className='text' type="text" required/>
-				}
-				
-			</div>
-			<div className="formBottom">
-				<label htmlFor="image"> <img src={photo} alt=""/> <p>Photo/Video</p> </label>
-				<input type="file" id="image" name="file" accept="image/*" className="file-custom" required/>
-				<input  name="uid" value={userId} hidden readOnly={true}/>
-				<button
-					className="sub-btn btn-lg btn-primary pull-xs-right"
-					type="submit">
-					Post
-				</button>
-			</div>
-		</form>
-		</>
+			}
+			<form onSubmit={submitForm} encType="multipart/form-data" className="post-form">
+				<div className="formTop">
+					<div className='profileImage'>
+						<img src={avatar} alt="profile logo"/>
+					</div>
+					{textArea ? <InputEmoji
+						className='text'
+						value={content}
+						onChange={setContent}
+						placeholder="What's happening?"
+						required
+					/>
+					:
+					<input onClick={expandTextArea} className='text' type="text" required/>
+					}
+					
+				</div>
+				{image && <div className="picture-preview">
+					<div className="top-image-preview">
+						<img className="preview" src={image} alt="preview" />
+					</div>
+					<p onClick={cancelPost}> X </p>
+					</div>}
+				<div className="formBottom">
+					<label onClick={expandTextArea} htmlFor="image"> <img src={photo} alt=""/> <p>Photo/Video</p>  </label>
+					{!textArea ? <div className="emoji" onClick={expandTextArea}>  <img src={emoji} alt=""/> <p>Feeling</p> </div> : null}
+					<input type="file" id="image" name="file" accept="image/*" className="file-custom" onChange={onChangeFile} required/>
+					<input name="uid" value={userId} hidden readOnly={true}/>
+					<button
+						className="sub-btn btn-lg btn-primary pull-xs-right"
+						type="submit">
+						Post
+					</button>
+				</div>
+			</form>
+		</div>
 	)
 }
 
