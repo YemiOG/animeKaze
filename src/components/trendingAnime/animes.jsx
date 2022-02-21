1e02eb2c34d7595dbfc330e1da0d3d86425c2339
Faruq <faruqabdulsalam@yahoo.com>
2022-02-21 21:15:46 +0100

    Create anime content

---
+++ src/components/trendingAnime/animes.jsx
@@ -0,0 +1,110 @@
+const Img1 = 'https://res.cloudinary.com/nagatodev/image/upload/v1645218015/Trending%20animes/attack-on-titan.jpg'
+const Img2 = 'https://res.cloudinary.com/nagatodev/image/upload/v1645218019/Trending%20animes/One-Piece.jpg'
+const Img3 = 'https://res.cloudinary.com/nagatodev/image/upload/v1645218026/Trending%20animes/Demon-Slayer-Entertainment-District.jpg'
+const Img4 = 'https://res.cloudinary.com/nagatodev/image/upload/v1645218021/Trending%20animes/my-hero-academia.jpg'
+const Img5 = 'https://res.cloudinary.com/nagatodev/image/upload/v1645218018/Trending%20animes/jujustu%20kaisen.jpg'
+const Img6 = 'https://res.cloudinary.com/nagatodev/image/upload/v1645218016/Trending%20animes/Boruto.jpg'
+const Img7 = 'https://res.cloudinary.com/nagatodev/image/upload/v1645218023/Trending%20animes/vinland_saga.jpg'
+const Img8 = 'https://res.cloudinary.com/nagatodev/image/upload/v1645218022/Trending%20animes/fireforce.jpg'
+const Img9 = 'https://res.cloudinary.com/nagatodev/image/upload/v1645218018/Trending%20animes/kingdom-Ran.jpg'
+const Img10 = 'https://res.cloudinary.com/nagatodev/image/upload/v1645218020/Trending%20animes/Tokyo-Revengers.jpg'
+const Img11 = 'https://res.cloudinary.com/nagatodev/image/upload/v1645218016/Trending%20animes/blackClover.jpg'
+
+
+const animes = [
+    {
+      id: 1,
+      img:Img1,
+      title: "Attack on Titan",
+      content: "When man-eating Titans first appeared 100 years ago, humans found safety behind massive walls that"
+              + "stopped the giants in their tracks. But the safety they have had for so long is threatened when a"
+              + "colossal Titan smashes through the barriers, causing a flood of the giants into what had been the human"
+    },
+    {
+      id: 2,
+      img:Img2,
+      title: "One Piece",
+      content: "One Piece is a Japanese manga series written and illustrated by Eiichiro Oda. It has been serialized in"
+               + "Shueisha's shōnen manga magazine Weekly Shōnen Jump since July 1997, with its individual chapters compiled"
+               + "into 101 tankōbon volumes as of December 2021."
+    },
+    {
+      id: 3,
+      img:Img3,
+      title: "Demon Slayer",
+      content: "Demon Slayer: Kimetsu no Yaiba is a Japanese manga series written and illustrated by Koyoharu Gotouge"
+               + "It follows teenage Tanjiro Kamado, who strives to become a demon slayer after his family was slaughtered"
+               + "and his younger sister Nezuko turned into a demon."
+    },
+    {
+      id: 4,
+      img:Img4,
+      title: "My Hero Academia",
+      content: "My Hero Academia is a Japanese superhero manga series written and illustrated by Kōhei Horikoshi."
+               +"The story follows Izuku Midoriya, a boy born without superpowers in a world where they have become commonplace,"
+               + "but who still dreams of becoming a superhero himself."
+    },
+    {
+      id: 5,
+      img:Img5,
+      title: "Jujustu Kaisen",
+      content: "Hardship, regret, shame: the negative feelings that humans feel become curses that lurk in our everyday lives."
+              + "Curses run rampant throughout the world, capable of leading people to terrible misfortune and even death."
+              + "What's more, the curses can only be exorcised by another curse."
+    },
+    {
+      id: 6,
+      img:Img6,
+      title: "Boruto",
+      content: "Boruto Uzumaki (うずまきボルト, Uzumaki Boruto) is a shinobi from Konohagakure's Uzumaki clan and a direct descendant"
+              + "of the Hyūga clan through his mother. While initially resentful of his father and his absence since becoming Hokage,"
+              + "Boruto eventually comes to respect his father and duties. Regardless, he vows to instead become a shinobi like his mentor."
+    },
+    {
+      id: 7,
+      img:Img7,
+      title: "Vinland Saga",
+      content: "Young Thorfinn grew up listening to the stories of old sailors that had traveled the ocean and reached the place of legend, Vinland."
+              + "It's said to be warm and fertile, a place where there would be no need for fighting—not at all like the frozen village in Iceland where"
+              + "he was born, and certainly not like his current life as a mercenary. War is his home now. Though his father once told him, \"You have no"
+              + "enemies, nobody does. There is nobody who it's okay to hurt,\" as he grew, Thorfinn knew that nothing was further from the truth."
+    },
+    {
+      id: 8,
+      img:Img8,
+      title: "Fire Force",
+      content: "In Year 198 of the Solar Era in Tokyo, Special Fire Force's are fighting against a phenomenon called Spontaneous Human Combustion where"
+              + "humans beings are turned into living infernos called “Infernals.” While the Infernals are First Generation cases of spontaneous human"
+              + "combustion, later generations possess the ability to manipulate flames while retaining human form. "
+    },
+    {
+      id: 9,
+      img:Img9,
+      title: "Kingdom",
+      content: "China’s Warring States period, a raging dragon that would raze the land for 500 years, saw many kingdoms rise and fall, making way for"
+              + "the next generation of kings and generals to fight for supremacy. Eventually, seven powerful states emerged from the endless cycle of warfare."
+              + "In the kingdom of Qin, Li Xin, a war-orphaned slave, trains vigorously with fellow slave and best friend, Piao, who shares his proud dream of"
+              + "one day becoming a Great General of the Heavens. However, the two are suddenly forced to part ways when Piao is recruited to work in the royal"
+              + "palace by a retainer of the King."
+    },
+    {
+      id: 10,
+      img:Img10,
+      title: "Tokyo Revengers",
+      content: "Takemichi Hanagaki's life is at an all-time low. Just when he thought it couldn't get worse, he finds out that Hinata Tachibana, his ex-girlfriend,"
+               + "was murdered by the Tokyo Manji Gang: a group of vicious criminals that has been disturbing society's peace for quite some time."
+               + "Wondering where it all went wrong, Takemichi suddenly finds himself travelling through time, ending up 12 years in the past—when"
+               + "he was still in a relationship with Hinata. Realizing he has a chance to save her, Takemichi resolves to infiltrate the Tokyo Manji"
+               + "Gang and climb the ranks in order to rewrite the future and save Hinata from her tragic fate."
+    },
+    {
+      id: 11,
+      img:Img11,
+      title: "Black Clover",
+      content: "The story follows Asta, a young boy born without any magic power. This is unknown to the world he lives in because seemingly everyone has some sort"
+               + "of magic power. With his fellow mages from the Black Bulls, Asta plans to become the next Wizard King."
+    },
+
+  ];
+  
+  export default animes
