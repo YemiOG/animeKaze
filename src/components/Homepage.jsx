26898f17cb2f18b94fa0263f479fb8b1690588c8
Faruq <faruqabdulsalam@yahoo.com>
2022-02-22 21:54:43 +0100

    Add caption to homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -136,15 +136,17 @@ function Home(){
 
 		
     return (
-        <div className="home-page">
+		<div className="home">
 			<div className="top-title">
 				Feed
 			</div>
-			<CreatePost post={getPosts}/>
-			{posts && posts.map(posts => <Posts key={posts.id} id={posts.id} content={posts.content} likeCount={posts.likes} 
-												image={posts.image} like={handlePost} interested={notInterested} 
-												report={reportPost} userLiked={posts.user_liked} avatar={posts.avatar} 
-												poster={posts.poster} fname={posts.fname} lname={posts.lname} unfollow={unfollowUser}/>)}
+			<div className="home-page">
+				<CreatePost post={getPosts}/>
+				{posts && posts.map(posts => <Posts key={posts.id} id={posts.id} content={posts.content} likeCount={posts.likes} 
+													image={posts.image} like={handlePost} interested={notInterested} 
+													report={reportPost} userLiked={posts.user_liked} avatar={posts.avatar} 
+													poster={posts.poster} fname={posts.fname} lname={posts.lname} unfollow={unfollowUser}/>)}
+			</div>
         </div>
     )
 }
