caced46b0fb4e406dd1108dd9c724220a6ff0970
Faruq <faruqabdulsalam@yahoo.com>
2022-03-17 08:38:15 +0100

    Import notifications and community components

--- src/components/application/AppRouter.js
+++ src/components/application/AppRouter.js
@@ -24,6 +24,12 @@ import RequestPasswordChange from "../Auth/RequestChangePassword"
 import Profile from "../profile/Profile"
 import Follow from "../follow/followerList"
 
+//notification
+import Notification from '../notification/notification'
+
+//notification
+import Community from '../community/community'
+
 //error page
 import PageNotFound from '../error/pageNotFound'
 
@@ -33,8 +39,11 @@ function AppRouter() {
 	const profileLocation = location.pathname.includes('user') && !location.pathname.includes('follow')
 	const followerLocation = location.pathname.includes('user') && location.pathname.includes('follower')
 	const followingLocation = location.pathname.includes('user') && location.pathname.includes('followed')
+	const sideBarDisplay = location.pathname.includes('notification') || location.pathname.includes('community') 
+	const loginLocation = location.pathname.includes('login') || location.pathname.includes('register')
 
-	// console.log(profileLocation)
+	// console.log(sideBarDisplay)
+	// console.log(location.pathname)
 	// console.log(followerLocation)
 	// console.log(followingLocation)
 	
@@ -64,7 +73,7 @@ function AppRouter() {
 		<UserContext.Provider value={{token, userInfo, appState, setAppState, setUserInfo, removeToken, setToken}}>
 			<Header />
 			<div className='App'>
-				<Sidebar/> 
+				{!loginLocation && <Sidebar/> }
 				{/* {console.log(userInfo.uzer)} */}
 				{console.log(location.pathname)}
 				{/* username: {userInfo.currentUser} */}
@@ -72,6 +81,8 @@ function AppRouter() {
 					<Route exact path="/login" element={<Login/>}></Route>
 					<Route exact path="/register" element={<Register/>}></Route>
 					<Route exact path="/" element={<Base />}></Route> 
+					<Route exact path="/notifications" element={<Notification />}></Route> 
+					<Route exact path="/community" element={<Community />}></Route> 
 					<Route exact path="/accounts/password/reset/" element={<RequestPasswordChange/>}></Route>
 					<Route exact path="/explore" element={
 								<AuthedRoute >
@@ -102,7 +113,7 @@ function AppRouter() {
 								}/>}
 					<Route path='*' element={<PageNotFound />}/>
 				</Routes> 				
-				{token && <Trend />}
+				{(token && !sideBarDisplay)  && <Trend />}
 				{/* <Footer /> */}
 			</div>
 		</UserContext.Provider>
