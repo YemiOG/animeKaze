a28d7117e6ccc10fbf3bc70d504707699226c88c
Faruq <faruqabdulsalam@yahoo.com>
2022-03-21 11:52:53 +0100

    Add social media svg to profile page

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -8,6 +8,14 @@ import CreatePost from "../posts/createPost"
 import Posts from "../posts/Posts"
 import EditProfile from "./EditProfile"
 
+// import svg icons
+import { ReactComponent as Dob } from '../../images/svg/dob.svg'
+import { ReactComponent as Facebook } from '../../images/svg/facebook.svg'
+import { ReactComponent as Instagram } from '../../images/svg/instagram.svg'
+import { ReactComponent as Twitter } from '../../images/svg/twitter.svg'
+import { ReactComponent as User } from '../../images/svg/user.svg'
+import { ReactComponent as Locate } from '../../images/svg/location.svg'
+
 function Profile() {
     // let navigate = useNavigate();
     const location = useLocation();
@@ -26,9 +34,16 @@ function Profile() {
       email:"",
       firstname:"",
       lastname:"",
+      avatar: null,
       followers_cnt:null,
       following_cnt:null,
-      posts:null
+      posts:null,
+      gender:"",
+      location:"",
+      twitter:"",
+      facebook:"",
+      instagram:"",
+      dob:"",
     })
     const userId = JSON.parse(window.localStorage.getItem("cuid"))
     const uzer = location.pathname.split("/")[2]
@@ -89,6 +104,7 @@ function Profile() {
             email:response.data.user.email,
             firstname:response.data.user.firstname,
             lastname:response.data.user.lastname,
+            avatar:response.data.user.avatar,
             about_me: response.data.user.about_me,
             followers_cnt: response.data.user.follower_count,
             following_cnt: response.data.user.followed_count,
@@ -125,6 +141,9 @@ function Profile() {
             console.log(error.response)
             console.log(error.response.status)
             console.log(error.response.headers)
+            }
+            if (error.response.status === 401 || error.response.status === 422){
+              removeToken()
             }
         })  
     }
@@ -185,46 +204,89 @@ function Profile() {
     return (
       <>
         {(!noUser) ?
-        <div className="profilePage">
-          {idMatch && <CreatePost/>}
-          <p style={{ textAlign: 'center', fontSize: '30px' }}>
-            Username:{profile.username}
-          </p>
-          <p style={{ textAlign: 'center', fontSize: '30px' }}>
-            Info:{profile.about_me}
-          </p>
-          <p style={{ textAlign: 'center', fontSize: '30px' }}>
-            Folllowers:<Link to='followers'> {profile.followers_cnt} </Link>
-          </p>
-          <p style={{ textAlign: 'center', fontSize: '30px' }}>
-            Following:<Link to='followed'> {profile.following_cnt} </Link>
-          </p>
-          <p style={{ textAlign: 'center', fontSize: '30px' }}>
-            Posts:{profile.posts}
-          </p>
-          {idMatch  ?
-              <button onClick={displayEdit}>
-                Edit Profile
-              </button>
-              :
-              token && (following ? 
-              <button onClick={unfollowUser}>
-                Following
-              </button>
-              :
-              <button onClick={followUser}>
-                Follow
-              </button>
-              )
-          }
-          {posts && posts.map(posts => <Posts key={posts.id} id={posts.id} content={posts.content} likeCount={posts.likes} image={posts.image} like={handlePost} interested={null} 
-                                          report={null} userLiked={posts.user_liked} avatar={posts.avatar} poster={posts.poster} 
-											                    fname={posts.fname} lname={posts.lname}/>)}
-          {editProfile && <EditProfile key={profile.id} username={profile.username} fname={profile.firstname} lname={profile.lastname} bio={profile.about_me} email={profile.email} 
-                            cancel={displayEdit} update={getProfile}/>}
-        </div>
-        :
-        <UserNotFound/>}
+        <div className="profile-page">
+          <div className="profile-header-avatar">
+            <div className="profile-header">
+                <img src="https://res.cloudinary.com/nagatodev/image/upload/c_scale,h_179,w_766/v1647505420/Profile/Default_header_background.png" 
+                      alt="profile header"/>
+            </div>
+            <div className='profile-page-image'>
+                <img src={profile.avatar} alt="profile logo"/>
+            </div>
+          </div>
+          
+          <div className="profile-info-topper">
+            <div className="profile-info-top">
+              <span>{profile.firstname} {profile.lastname}</span><span className="profile-info-top">@{profile.username}</span>
+              <p className="">
+                {profile.about_me}
+              </p>
+            </div>
+            {idMatch  ?
+                    <button className="profile-info-btn1" onClick={displayEdit}>
+                      Edit Profile
+                    </button>
+                    :
+                    token && (following ? 
+                    <button className="profile-info-btn2" onClick={unfollowUser}>
+                      Following
+                    </button>
+                    :
+                    <button className="profile-info-btn3" onClick={followUser}>
+                      Follow
+                    </button>
+                    )
+                }
+          </div>
+
+          <div className="profile-details"> 
+              <div className="detail-card">
+                <div className="top-detail-card">
+                  <p>
+                    <User fill="191E22"/> {profile.gender ? profile.gender : <span className="noDetail"> ------------------------------- </span>} 
+                  </p>
+                  <p>
+                    <Dob fill="191E22"/>{profile.dob ? <>Born profile.dob </> : <span className="noDetail"> ------------------------------- </span>}
+                  </p>
+                  <p>
+                    <Locate fill="191E22"/> {profile.location ? profile.location : <span className="noDetail"> ------------------------------- </span>}
+                  </p>
+                  <p>
+                    <Instagram fill="191E22"/> {profile.instagram ? profile.instagram : <span className="noDetail"> ------------------------------- </span>}
+                  </p>
+                  <p>
+                    <Twitter fill="191E22"/> {profile.twitter ? profile.twitter : <span className="noDetail"> ------------------------------- </span>}
+                  </p>
+                  <p>
+                    <Facebook fill="191E22"/> {profile.facebook ? profile.facebook : <span className="noDetail"> ------------------------------- </span>}
+                  </p>
+                </div>
+                  <div className="post-follow">
+                    <p>
+                      <Link to='followed'> <span> {profile.following_cnt} </span> Following</Link> 
+                    </p>
+                    <p>
+                      <Link to='followers'> <span> {profile.followers_cnt} </span> {profile.followers_cnt.length > 1 ? <>Followers</> : <>Follower</>}</Link> 
+                    </p>
+                    <p>
+                      <span>Posts:</span> <span>{profile.posts}</span>
+                    </p>
+                  </div>
+              </div>
+              <div className="post-detail">
+                {idMatch && <CreatePost/>}
+                <div className="profile-posts">
+                  {posts && posts.map(posts => <Posts key={posts.id} id={posts.id} content={posts.content} likeCount={posts.likes} image={posts.image} like={handlePost} interested={null} 
+                                                  report={null} userLiked={posts.user_liked} avatar={posts.avatar} poster={posts.poster} 
+                                                  fname={posts.fname} lname={posts.lname}/>)}
+                </div>
+                {editProfile && <EditProfile key={profile.id} username={profile.username} fname={profile.firstname} lname={profile.lastname} bio={profile.about_me} email={profile.email} 
+                                  cancel={displayEdit} update={getProfile}/>}
+              </div>
+          </div>
+        </div>  
+          :
+          <UserNotFound/>}
       </>
     );
 }
