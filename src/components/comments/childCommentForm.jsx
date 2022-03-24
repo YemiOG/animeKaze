e7456e9163f321b9706d317086accd39d87930aa
Faruq <faruqabdulsalam@yahoo.com>
2022-03-24 08:36:50 +0100

    Add header to child comment form

--- src/components/comments/childCommentForm.jsx
+++ src/components/comments/childCommentForm.jsx
@@ -1,28 +1,33 @@
 import { ReactComponent as Post } from '../../images/svg/post.svg'
 
 function ChildCommentForm(props) {
-	
+
 	return (
-		<form className="comment-form" onSubmit={props.commentform}>
-				<div className='profile-image'>
-					<img src={props.avatar} alt="profile logo"/>
-				</div>
-                <textarea
-                    className=""
-                    type="text"
-                    name="content"
-                    placeholder="Reply comment..."
-					onFocus={props.focus}
-					onBlur={props.focus}
-					rows={props.row}
-					value={props.new}
-					onChange={props.change} 
-					required
-					/>
-				<button className="comment-btn">
-					<Post />
-				</button>
-	</form>
+		<>
+			<p> Replying comment</p>
+			<form className="comment-form" onSubmit ={props.submitChild}>
+					<div className='profile-image'>
+						<img src={props.avatar} alt="profile logo"/>
+					</div>
+					<textarea
+						className=""
+						type="text"
+						name="content"
+						placeholder="Reply comment..."
+						// onFocus={props.focus}
+						onBlur={props.focus}
+						rows={props.row}
+						value={props.new}
+						onChange={props.change} 
+						required
+						autoFocus 
+						/>
+
+					<button className="comment-btn" disabled={props.poss} >
+						<Post />
+					</button>
+			</form>
+		</>
 	);
   }
   
