368cfe027b1c77f46b8cb28a438cf681b12b858e
Faruq <faruqabdulsalam@yahoo.com>
2022-03-25 07:48:06 +0100

    Add useEffect hook

--- src/components/posts/createPost.jsx
+++ src/components/posts/createPost.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useContext} from "react";
+import React, {useState, useEffect, useContext} from "react";
 import { UserContext } from '../contexts/userContext';
 import axios from "axios";
 import InputEmoji from "react-input-emoji";
